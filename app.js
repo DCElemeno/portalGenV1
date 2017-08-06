3675f7f43b2397eb16b7baa20945e28576390157
dcoughlin <dcelemeno@gmail.com>
2017-08-06 15:50:39 -0400

    starting planning of everything

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 //setup app
-var myApp = angular.module('retrieve',[]);
+var myApp = angular.module('portal',[]);
 
 //main controller
 myApp.controller('main', function($scope) {    
 
+    //test
     $scope.foo = "bar";
+
+    //config items are going to be listed in an object here
+    $scope.config = {};
 
 });
